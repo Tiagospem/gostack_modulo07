c39bd92f64177a7538c8ca225afbf73d4efd4381
Tiagospem <tiagospem@gmail.com>
2019-09-03 16:20:04 -0300

    change quantity

--- src/store/modules/cart/actions.js
+++ src/store/modules/cart/actions.js
@@ -1,13 +1,21 @@
 export function addToCart(product) {
   return {
-    type: '@cart/add',
+    type: '@cart/ADD',
     product,
   }
 }
 
 export function removeFromCart(id){
   return {
-    type: '@cart/remove',
+    type: '@cart/REMOVE',
     id
+  }
+}
+
+export function updateAmount(id, amount){
+  return {
+    type: '@cart/UPDATE_AMOUNT',
+    id,
+    amount,
   }
 }
