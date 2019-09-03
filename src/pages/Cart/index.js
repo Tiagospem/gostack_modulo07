4d64dee2df5ea0928d7c83ef0f8aa2e2ca4e44bd
Tiagospem <tiagospem@gmail.com>
2019-09-03 12:24:22 -0300

    Home & Cart styles + fake API + Intl.NumberFormat

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {MdRemoveCircleOutline, MdAddCircleOutline, MdDelete} from 'react-icons/md';
+import {Container, ProductTable, Total} from './styles';
+
+function Cart() {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src="https://static.netshoes.com.br/produtos/tenis-infantil-nike-md-runner-2/26/D12-2124-226/D12-2124-226_detalhe1.jpg?resize=280:280" alt="" />
+            </td>
+            <td>
+              <strong>Tenis massa pakas</strong>
+              <span>R$125,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$250,00</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar Pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 12521,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
+}
+
+export default Cart;
