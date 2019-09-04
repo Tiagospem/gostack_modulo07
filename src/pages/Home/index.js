82aeac474dc741fa5bc8a327f5348040c323831e
Tiagospem <tiagospem@gmail.com>
2019-09-04 16:44:50 -0300

    refactor home using hooks

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import {connect} from 'react-redux';
 import {MdAddShoppingCart} from 'react-icons/md';
 import {bindActionCreators} from 'redux';
@@ -7,48 +7,45 @@ import {ProductList} from './styles';
 import api from '../../services/api';
 import { formatPrice } from '../../util/format'
 
-class Index extends Component {
-  state = {
-    products: [],
-  };
+function Index({ amount, addToCartRequest }) {
 
-  async componentDidMount() {
-    const response = await api.get('products');
-    const data = response.data.map(product => ({
-      ...product,
-      priceFormated: formatPrice(product.price)
-    }));
-    this.setState({products: data});
-  };
+  const [products, setProducts] = useState([]);
 
-  handleAddProduct = id => {
-    const { addToCartRequest } = this.props;
+  // executa uma unica vez qd o componente for montado
+  useEffect(() => {
+    async function loadProducts(){
+      const response = await api.get('products');
+      const data = response.data.map(product => ({
+        ...product,
+        priceFormated: formatPrice(product.price)
+      }));
+      setProducts(data);
+    }
+    loadProducts();
+  }, []);
+
+  function handleAddProduct(id) {
     addToCartRequest(id);
-  };
-
-  render() {
-    const {products} = this.state;
-    const {amount} = this.props;
-    return (
-      <ProductList>
-        {products.map(product => (
-          <li key={product.id}>
-            <img src={product.image} alt={product.title} />
-            <strong>{product.title}</strong>
-            <span>{product.priceFormated}</span>
-            <button type="button" onClick={() => this.handleAddProduct(product.id)}>
-              <div>
-                <MdAddShoppingCart size={16} color="#fff" />
-                {' '}
-                {amount[product.id] || 0}
-              </div>
-              <span>Adicionar ao Carrinho</span>
-            </button>
-          </li>
-        ))}
-      </ProductList>
-    );
   }
+  return (
+    <ProductList>
+      {products.map(product => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormated}</span>
+          <button type="button" onClick={() => handleAddProduct(product.id)}>
+            <div>
+              <MdAddShoppingCart size={16} color="#fff" />
+              {' '}
+              {amount[product.id] || 0}
+            </div>
+            <span>Adicionar ao Carrinho</span>
+          </button>
+        </li>
+      ))}
+    </ProductList>
+  );
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators(CartActions, dispatch);
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-
+// rodar o json server: json-server server.json -p 3333
+// rodar o json server com delay: json-server server.json -p 3333 -d 2000
 const api = axios.create({
   baseURL: 'http://localhost:3330'
 });
