3926b43a59c01ab69da58355cac6074ebae3f193
Tiagospem <tiagospem@gmail.com>
2019-09-04 14:04:21 -0300

    added toastfy

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './config/ReactotronConfig';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 import store from './store';
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
@@ -14,6 +15,7 @@ function App() {
         <Header />
         <Routes />
         <GlobalStyle />
+        <ToastContainer />
       </BrowserRouter>
     </Provider>
   );
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,5 +1,7 @@
+import 'react-toastify/dist/ReactToastify.css';
 import {createGlobalStyle} from 'styled-components';
 import bg from '../assets/images/bg.svg';
+
 
 export default createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
