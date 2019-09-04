3d646c5325fd2e2268a7f79d6fde803f86d5c66b
Tiagospem <tiagospem@gmail.com>
2019-09-04 14:22:16 -0300

    change quantity in saga

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -14,7 +14,7 @@ export default function cart(state = [], action){
           draft.splice(productIndex, 1);
         }
       });
-    case '@cart/UPDATE_AMOUNT':
+    case '@cart/UPDATE_AMOUNT_SUCCESS':
     {
       if(action.amount === 0){
         return produce(state, draft => {
@@ -23,9 +23,6 @@ export default function cart(state = [], action){
             draft.splice(productIndex, 1);
           }
         });
-      }
-      if(action.amount < 0){
-        return state;
       }
       return produce(state, draft => {
         const productIndex = draft.findIndex(p => p.id === action.id);
